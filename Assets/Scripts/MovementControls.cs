341c1325955c92f3c06a402b92b50922e53e4dc7
Christopher Boyce <57403492+chris-boyce@users.noreply.github.com>
2022-04-07 15:35:45 +0100

    YAya

--- Assets/Scripts/MovementControls.cs
+++ Assets/Scripts/MovementControls.cs
@@ -6,11 +6,14 @@ using UnityEngine.InputSystem;
 public class MovementControls : MonoBehaviour
 {
     VivecontrolUwu ViveController;
+    
     private Vector2 movementInput;
     private Rigidbody rb;
     public GameObject Offset;
     private float offsetDistance;
-
+    public Transform CurrentForwardDirection;
+    public Transform cachedForwardDirection;
+    
 
     void Start()
     {
@@ -18,13 +21,6 @@ public class MovementControls : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        rb.AddForce(new Vector3(movementInput.x *2 ,0,0), ForceMode.Impulse);
-        rb.AddForce(new Vector3(0, 0, movementInput.y*2), ForceMode.Impulse);
-
-    }
     private void FixedUpdate()
     {
         RaycastHit hit;
@@ -35,11 +31,36 @@ public class MovementControls : MonoBehaviour
             if(hit.transform.tag == "Ground")
             {
                 offsetDistance = hit.distance;
-                Offset.transform.position = new Vector3(Offset.transform.position.x, -offsetDistance + 1.25f , Offset.transform.position.z) ;
+                Offset.transform.position = new Vector3(Offset.transform.position.x, -offsetDistance + 1.75f , Offset.transform.position.z) ;
 
 
             }
         }
+        if(movementInput.x < 0.1 || movementInput.y < 0.1)
+        {
+            cachedForwardDirection = CurrentForwardDirection;
+        }
+
+
+
+        if(movementInput.x < 0.2)
+        {
+            rb.AddRelativeForce(cachedForwardDirection.transform.forward + new Vector3(movementInput.x * 2, 0, 0), ForceMode.Impulse);
+        }
+        if(movementInput.x > 0.2)
+        {
+            rb.AddRelativeForce(-cachedForwardDirection.transform.forward + new Vector3(movementInput.x * 2, 0, 0), ForceMode.Impulse);
+        }
+
+        if(movementInput.y < 0.2)
+        {
+
+        }
+        rb.AddRelativeForce(cachedForwardDirection.transform.forward + new Vector3(0, 0, movementInput.y * 2), ForceMode.Impulse);
+
+
+        //rb.AddForce(new Vector3(movementInput.x * 2, 0, 0), ForceMode.Impulse);
+        //rb.AddForce(new Vector3(0, 0, movementInput.y * 2), ForceMode.Impulse);
     }
 
     public void OnMove(InputAction.CallbackContext context)
